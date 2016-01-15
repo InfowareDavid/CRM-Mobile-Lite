10f3e0539499a67fdb107654acec6f95c6164c37
infoware <Infoware@infowares-macbook-pro.local>
2016-01-15 17:05:59 +0800

    Initial Commit

---
+++ CRM Mobile Lite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CRM Mobile Lite
+//
+//  Created by infoware on 15/1/16.
+//  Copyright © 2016年 infoware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
